46b3e083063f2b32f9fa9df0c173bb10aa4f7b9d
guliyevaa <12545@holbertonstudents.com>
2025-12-15 08:50:35 -0500

    .

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps aux -u "$1" -o user,pid,pcpu,pmem,vsz,rss,tty,stat,start,time,cmd | grep -v " 0  0 "
+ps aux -u "$1" -o pid,user,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd --no-headers | grep -v " 0  0 "
