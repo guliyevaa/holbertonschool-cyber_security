f1d3b02849568d74c2cc86cab94f11004526ee7a
guliyevaa <12545@holbertonstudents.com>
2025-12-26 06:24:16 -0500

    .

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | while read s; do ip=$(dig +short "$s" | head -n1); [ -z "$ip" ] && ip="N/A"; echo "$s,$ip"; done > "$1.txt"
+subfinder -d "$1" -silent -nW -oI -o "$1.txt"
