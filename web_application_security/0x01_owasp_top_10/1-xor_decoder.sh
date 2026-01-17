04e323a9358764beba31bcd4dd53d473b1bd8e02
guliyevaa <12545@holbertonstudents.com>
2026-01-17 06:50:23 -0500

    .

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Remove {xor}, decode base64, XOR each byte with 0x5A
-printf "%s" "${1#\{xor\}}" | base64 -d | perl -lpe '$_=join("", map { chr(ord($_)^0x5A) } split("", $_))'
+# remove {xor}, decode base64, XOR each byte with 0x5A
+perl -e 'use MIME::Base64; $s=decode_base64(substr($ARGV[0],5)); print join("", map { chr(ord($_)^0x5A) } split("", $s))' "$1"
