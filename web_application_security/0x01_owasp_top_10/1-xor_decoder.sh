500638847ffcf3e4a179dca0d03e41d8c66b3d11
guliyevaa <12545@holbertonstudents.com>
2026-01-17 06:47:46 -0500

    .

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1" | sed 's/{xor}//' | base64 -d | perl -pe 's/(.)/chr(ord($1)^0x5a)/ge'
+printf "%s" "${1#\{xor\}}" | base64 -d | gawk '{ for(i=1;i<=length;i++) printf "%c", xor(ord(substr($0,i,1)),90) }'
