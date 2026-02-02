ab16acc32fb41531990585156388ac231de64d2c
guliyevaa <12545@holbertonstudents.com>
2026-02-02 16:09:48 -0500

    .

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1; john --show "$1" | awk -F: '{print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" | john --show "$1" | grep ":" | cut -d: -f2 > 4-password.txt
