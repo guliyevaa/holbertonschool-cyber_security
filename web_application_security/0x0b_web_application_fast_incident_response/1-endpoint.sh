da9afc17c1c6dd15b1e73f22b8d45dba180bb4eb
guliyevaa <12545@holbertonstudents.com>
2026-04-07 09:00:00 -0400

    .

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-awk -F'"' '{print $2}' logs.txt | awk '{print $2}' | sort | uniq -c | sort -nr | head -n 1
+
+# Extract requested URLs, count them, sort, and print the most frequent one
+awk -F'"' '{print $2}' logs.txt | awk '{print $2}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
