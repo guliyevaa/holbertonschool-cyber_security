3fd837d4f26ab0f9654ff93668fc6eb28a899fc4
guliyevaa <12545@holbertonstudents.com>
2026-04-07 09:44:58 -0400

    .

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-awk -v ip=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}') '$1==ip {match($0, /"[^"]*"$/, a); print a[0]}' logs.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'
+attacker_ip=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}')
+awk -v ip="$attacker_ip" '$1==ip {match($0, /"[^"]*"$/, a); print a[0]}' logs.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'
