623d5727b0575ccbe8671025226758ef73a349b8
guliyevaa <12545@holbertonstudents.com>
2026-04-13 08:21:35 -0400

    .

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
-#!bin/bash
-grep "sshd.*Accepted password" auth.log | awk '{print $(NF-3)}' | sort -u | wc -l
+#!/bin/bash
+grep -E 'sshd.*Accepted password for root' auth.log | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u | wc -l
